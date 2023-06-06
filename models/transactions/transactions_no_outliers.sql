5b12cf9f985277cd35b20fbcfc04b116a41f8fd2
Félix Hernández Vieyra <feluhv@gmail.com>
2023-06-06 20:14:18 +0000

    add: transactions_no_outliers.sql

---
+++ models/transactions/transactions_no_outliers.sql
@@ -0,0 +1,15 @@
+SELECT t.*
+FROM `transformed_data_dbt.transactions_to_lowercase` AS t
+JOIN (
+  SELECT
+    transactions_currency,
+    APPROX_QUANTILES(amount_usd, 4)[OFFSET(1)] AS first_quartile,
+    APPROX_QUANTILES(amount_usd, 4)[OFFSET(3)] AS third_quartile
+  FROM `transformed_data_dbt.transactions_to_lowercase`
+  GROUP BY transactions_currency
+) AS quartiles
+ON t.transactions_currency = quartiles.transactions_currency
+WHERE
+  (t.amount_usd >= quartiles.first_quartile - 1.5 * (quartiles.third_quartile - quartiles.first_quartile))
+  AND (t.amount_usd <= quartiles.third_quartile + 1.5 * (quartiles.third_quartile - quartiles.first_quartile))
+ORDER BY transactions_currency;
