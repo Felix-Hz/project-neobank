9a26034ab81d7599bcb3b06b9b2a4c5fcf08ce73
Félix Hernández Vieyra <feluhv@gmail.com>
2023-06-06 21:18:28 +0000

    fix: 2_transactions_cities

--- models/transactions/2_transactions_cities_standarized.sql
+++ models/transactions/2_transactions_cities_standarized.sql
@@ -1,19 +1,27 @@
+WITH cte AS (
+  SELECT
+    *,
+    CASE
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'new york') THEN 'new york'
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'praha') THEN 'praga'
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'paris') THEN 'paris'
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'tfl') THEN 'london'
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'london') THEN 'london'
+      WHEN REGEXP_CONTAINS(ea_merchant_city, r'dublin') THEN 'dublin'
+      WHEN ea_merchant_city = '4029357733' THEN 'london'
+      WHEN ea_merchant_city = '800-592-8996' THEN 'san francisco'
+      WHEN ea_merchant_city = 'abudhabi' THEN 'abu dhabi'
+      WHEN ea_merchant_city = 'dubai h' THEN 'dubai'
+      WHEN ea_merchant_city = 'cluj-napoca' THEN 'cluj napoca'
+      WHEN ea_merchant_city = 'palma de mall' THEN 'palma de mallorca'
+      WHEN ea_merchant_city = 'z?rich' THEN 'zurich'
+      ELSE ea_merchant_city
+    END AS ea_merchant_city_modif
+  FROM transformed_data_dbt.transactions_no_outliers
+)
 SELECT
-  * EXCEPT (ea_merchant_city)
-  , CASE
-    WHEN ea_merchant_city = 'praha' THEN 'praga'
-    WHEN ea_merchant_city LIKE 'praha%' THEN 'praga'
-    WHEN ea_merchant_city LIKE '%paris%' THEN 'paris'
-    WHEN ea_merchant_city = 'tfl.gov.uk/cp' THEN 'london'
-    WHEN ea_merchant_city = 'tfl travel ch' THEN 'london'
-    WHEN ea_merchant_city LIKE 'london%' THEN 'london'
-    WHEN ea_merchant_city = '4029357733' THEN 'london'
-    WHEN ea_merchant_city = '800-592-8996' THEN 'san francisco'
-    WHEN ea_merchant_city like '%dublin%' THEN 'dublin'
-    WHEN ea_merchant_city = 'cluj-napoca' THEN 'cluj napoca'
-    WHEN ea_merchant_city = 'palma de mall' THEN 'palma de mallorca'
-    WHEN ea_merchant_city = 'z?rich' THEN 'zurich'
-    ELSE ea_merchant_city
-  END AS ea_merchant_city,
-  COALESCE(ea_merchant_city, 'n/a') AS ea_merchant_city_standarized,
-FROM transformed_data_dbt.transactions_no_outliers
+  *,
+  COALESCE(ea_merchant_city_modif, 'n/a') AS ea_merchant_city_standarized
+FROM cte
+
+
